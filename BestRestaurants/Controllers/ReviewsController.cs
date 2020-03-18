2a01b1f7ddb9efd7ad0c0eeb863f9675610ef290
Michelle Morin <morin2m@gmail.com>
2020-03-18 15:36:13 -0700

    update controllers to accomodate review class
    co-authored-by: Co-Authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: Alex Skreen <skreenalex@gmail.com>

---
+++ BestRestaurants/Controllers/ReviewsController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BestRestaurants.Controllers
+{
+  public class ReviewsController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public ReviewsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    } 
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.RestaurantId = new SelectList(_db.Restaurants, "RestaurantId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Review review)
+    {
+      _db.Reviews.Add(review);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
