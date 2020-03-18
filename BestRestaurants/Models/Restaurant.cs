43736e60f24684e2c8e5f8f78252072169c22ccf
Alex Skreen <skreenalex@gmail.com>
2020-03-18 11:05:48 -0700

    initial commit
    co-authored-by: Michelle Morin <morin2m@gmail.com>
    co-authored-by: Todd Walraven <cod4247@yahoo.com>

---
+++ BestRestaurants/Models/Restaurant.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace BestRestaurants.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public int CuisineId { get; set; }
+    public string PriceRange { get; set; }
+
+    public virtual Cuisine Cuisine { get; set; }
+  }
+}
